f40a9735ccb178cc5d2015c6e19a490007fcd3bb
dewasatriawan <dewa@bsa.id>
2022-06-23 23:20:46 +0800

    register

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,13 @@
 <template>
-  <v-app>
-    <Nav></Nav>
-
-    <div class="container-fluid">
-      <div class="row">
-        <Menu></Menu>
-
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-          <div class="table-responsive">
-            <table class="table table-striped table-sm">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </main>
-      </div>
-    </div>
-  </v-app>
+<router-view></router-view>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Nav from './components/Nav.vue';
-import Menu from './components/Menu.vue';
+
 
 export default Vue.extend({
     name: "App",
-    components: { Nav, Menu }
 });
 </script>
 
